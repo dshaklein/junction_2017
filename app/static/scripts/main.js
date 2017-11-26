70c85e0b0a51de8bb2d9920518659188fc6a07cb
Shad <shadowagent1st@gmail.com>
2017-11-26 05:24:26 +0200

    emoji highlight fix button fix

--- app/static/scripts/main.js
+++ app/static/scripts/main.js
@@ -1,13 +1,15 @@
 var emojies = [];
     $( ".emoji-card" ).click(function() {
         var elem = $(this);
-        elem.toggleClass( "emoji-card-focus", !elem.is("focus"));
-        if (!elem.is(":focus")) {
+        elem.toggleClass( "emoji-card-focus", !elem[0].classList.contains("emoji-card-focus"));
+        if (!elem[0].classList.contains("emoji-card-focus")) {
             if ($.inArray(elem[0].id, emojies) == -1) {
                 emojies.push(elem[0].id);
             }
         }
     });
     $("#post-button").click(function () {
-       $.get( "/search/", {string: emojies.toString()} );
+       $.get( "/search", {string: emojies.toString()}, function (data) {
+           $( ".result" ).html(data);
+       });
     });
